9be374e53c6c96616de7f307af19dd76fdda4f69
Erick ngechu <ekngechu003@gmail.com>
2023-10-25 10:30:31 +0300

    markup updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,17 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      "Dark-Blue": "hsl(217, 28%, 15%)",
-      "Dark-Blue": "hsl(218, 28%, 13%)",
-      "Dark-Blue": "hsl(216, 53%, 9%)",
-      "Dark-Blue ": "hsl(219, 30%, 18%)",
+      "Dark-Blue-1": "hsl(217, 28%, 15%)",
+      "Dark-Blue-2": "hsl(218, 28%, 13%)",
+      "Dark-Blue-3": "hsl(216, 53%, 9%)",
+      "Dark-Blue-4": "hsl(219, 30%, 18%)",
 
-      "Cyan ": "hsl(176, 68%, 64%)",
+      Cyan: "hsl(176, 68%, 64%)",
       Blue: "hsl(198, 60%, 50%)",
       "Light-Red": " hsl(0, 100%, 63%)",
 
-      " White": " hsl(0, 0%, 100%)",
+      White: " hsl(0, 0%, 100%)",
+      "white-2": "hsla(0, 0%, 84%, 0.813)",
     },
   },
   plugins: [],
