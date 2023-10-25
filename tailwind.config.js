0a5f5c4812de059d2c6281040bcb25a8841be8e8
Erick ngechu <ekngechu003@gmail.com>
2023-10-25 09:23:49 +0300

    My first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "Dark-Blue": "hsl(217, 28%, 15%)",
+      "Dark-Blue": "hsl(218, 28%, 13%)",
+      "Dark-Blue": "hsl(216, 53%, 9%)",
+      "Dark-Blue ": "hsl(219, 30%, 18%)",
+
+      "Cyan ": "hsl(176, 68%, 64%)",
+      Blue: "hsl(198, 60%, 50%)",
+      "Light-Red": " hsl(0, 100%, 63%)",
+
+      " White": " hsl(0, 0%, 100%)",
+    },
+  },
+  plugins: [],
+};
