2cc4a013bd55159f8bdcf092aa367b6c147c7e5e
Erick ngechu <ekngechu003@gmail.com>
2023-10-26 02:23:12 +0300

    Readme upates

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const btnSubmit = document.querySelector(".btn-submit");
+const input = document.querySelector(".input");
+const form = document.querySelector("form");
+
+//Some more form validation to be included
+// btnSubmit.addEventListener("click", function (e) {
+//   //   e.preventDefault();
+
+//   const inputTxt = input.value;
+//   console.log(inputTxt);
+// });
